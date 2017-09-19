4233c8d5cc59b2ed44747ee0f4973571a20091cb
pariyar <sunilpariyar@hotmail.com>
2017-09-19 15:30:31 +1000

    working version

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import axios from 'axios';
 import './App.css';
 import Form from './components/Form'
 
-export default class App extends Component{
-    constructor(props){
+export default class App extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             infos: [],
-        }        
-    }    
+        }
+    }
 
     componentDidMount() {
         const that = this;
@@ -19,31 +19,33 @@ export default class App extends Component{
             url: 'http://localhost:8080/data',
             method: 'get',
             // baseURL: 'https://localhost:8080',
-        }).then(function(result){
-                var apiData = result.data;
-                that.setState({ infos: apiData 
-            })            
-        }).catch(function(error) {
-                console.log(error);
+        }).then(function (result) {
+            var apiData = result.data;
+            that.setState({
+                infos: apiData
             })
-    }    
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
 
     handleClickDelete = (data) => {
         axios({
             method: 'delete',
-            url: 'http://localhost:8080/data/:id',
-            params: {
-                id: data._id
-            }
-          })
-          var infosArray = this.state.infos;
-          var index = infosArray.indexOf(data);
-          infosArray.splice(index,1);
-          this.setState({infos: infosArray});
+            url: 'http://localhost:8080/data/' + data._id,
+            // params: {
+            //     id: data._id
+            // }
+        })
+        var infosArray = this.state.infos;
+        var index = infosArray.indexOf(data);
+        infosArray.splice(index, 1);
+        this.setState({ infos: infosArray });
     }
 
-    handleClickAdd = (params) => {
-         
+    handleClickAdd = (e, params) => {
+
+        e.preventDefault();
         const data = {
             name: params.name,
             address: params.address,
@@ -53,48 +55,71 @@ export default class App extends Component{
             method: 'post',
             url: 'http://localhost:8080/data/post',
             data
-          });
-          const { infos } = this.state;
-          this.setState({
-              infos: [ ...infos, data ]
-          })
-            console.log('success');
-    } 
-    
-    render(){
+        });
+        const { infos } = this.state;
+        this.setState({
+            infos: [...infos, data]
+        })
+        console.log('success');
+    }
+
+    // handleClickEdit = (data) => {
+    //     axios({
+    //         method: 'put',
+    //         url: 'http://localhost:8080/data/',
+    //         params: {
+    //             id: data._id
+    //         }
+    //     })
+    //     var infosArray = this.state.infos;
+    //     var index = infosArray.indexOf(data);
+    //     infosArray.splice(index, 1);
+    //     this.setState({ infos: infosArray });
+    // }
+
+    render() {
         const data = this.state.infos;
         var rowNum = 0;
         return (
-            <div>
-                <br/>
-                <Form handleClick={this.handleClickAdd} state={this.state.infos}/>
-                <br/>
-                <table style={{border:"1px solid", width:550}}>
-                    <tbody>
-                        <tr>
-                            <th>S.N</th>                            
-                            <th>Name</th>                    
-                            <th>Address</th>
-                            <th>Comment</th>
-                            <th>Operations</th>                            
-                        </tr>
-                        {data.map((data) =>
-                        <tr>
-                            <td>{rowNum++}</td>                            
-                            <td key={data._id}>{data.name}</td>
-                            <td>{data.address}</td>
-                            <td>{data.comment}</td>
-                            <td>
-                                <button>Edit</button>
-                                <button value={data._id} onClick={() => this.handleClickDelete(data)}>Delete</button>
-                            </td>                            
-                        </tr>                                            
-                        )}
-                        
-                    </tbody>
-                </table>
+            <div style={{float:"center",alignContent:"center"}}>
+                <div className="nav">
+                    <h1>API data manipulation....</h1>
+                </div>
+                <div className="dataTable">
+                   <div style={{display:"flex", justifyContent:"space-around"}}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>S.N</th>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Comment</th>
+                                <th>Operations</th>
+                            </tr>
+                            {data.map((data) =>
+                                <tr>
+                                    <td>{rowNum++}</td>
+                                    <td key={data._id}>{data.name}</td>
+                                    <td>{data.address}</td>
+                                    <td>{data.comment}</td>
+                                    <td>
+                                        <button>Edit</button>
+                                        <button value={data._id} onClick={() => this.handleClickDelete(data)}>Delete</button>
+                                    </td>
+                                </tr>
+                            )}
+
+                        </tbody>
+                    </table>
+                    </div>
+                    <div style={{display:"flex", justifyContent:"space-around"}}>
+                        <br />
+                        <Form handleClick={this.handleClickAdd} />
+                        <br />
+                    </div>
+                </div>
             </div>
-          );               
+        );
     }
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,16 +5,15 @@ import '../App.css';
 import axios from 'axios';
 import App from '../App.js'
 
-
-export default class Form extends Component{
-    constructor(props){
+export default class Form extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-                name: '',
-                address: '',
-                comment: ''            
+            name: '',
+            address: '',
+            comment: ''
         }
-    }   
+    }
 
     // handleClickAdd = (e) => {
     //     e.preventDefault();
@@ -35,29 +34,49 @@ export default class Form extends Component{
     //         console.log('success');
     // } 
 
-    handleChangeFor = (key) => (e) => {    
-      var state = {};
-      state[key] = e.target.value;
-      this.setState(state);
+    // handleChangeFor = (key) => (e) => {
+    //     var state = {};
+    //     state[key] = e.target.value;
+    //     this.setState(state);
+    // }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
     }
 
-    render(){
-        return(
+    render() {
+        const { name, address, comment } = this.state;
+        return (
             <div>
-                <form  onSubmit={() => this.props.handleClick(this.state)}>
-                    <input type="text" placeholder="Name" 
-                        value={this.state.name} 
-                        onChange={this.handleChangeFor('name')}
-                        id="name"/>
-                    <input type="text" placeholder="Address" 
-                        value={this.state.address} 
-                        onChange={this.handleChangeFor('address')}
-                        id="address"/>
-                    <input type="text" placeholder="Comment" 
-                        value={this.state.comment}
-                        onChange={this.handleChangeFor('comment')}
-                        id="comment"/>
-                    <input type="submit" value="Add"/>
+                <form onSubmit={(e) => this.props.handleClick(e,this.state)}>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={this.handleChange}
+                        id="name"
+                        name="name"
+                    />
+                    <input
+                        type="text" placeholder="Address"
+                        value={address}
+                        onChange={this.handleChange}
+                        id="address"
+                        name="address"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Comment"
+                        value={comment}
+                        onChange={this.handleChange}
+                        id="comment"
+                        name="comment"
+                    />
+                    <input type="submit" value="Add" />
                 </form>
             </div>
         );
